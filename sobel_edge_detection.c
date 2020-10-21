df653f6b762c9b379b800d1bb0522255db3db965
Fatma Zehra Çetin <37731704+fzehracetin@users.noreply.github.com>
2020-10-21 14:51:18 +0300

    Add files via upload

--- sobel_edge_detection.c
+++ sobel_edge_detection.c
@@ -9,6 +9,8 @@ typedef struct {
 	int height;
 	int maxGrayLevel;
 	int **imageData;
+	int **gx;
+	int **gy;
 } pgm;
 
 void init_out_image( pgm* out, pgm image){
@@ -18,14 +20,26 @@ void init_out_image( pgm* out, pgm image){
 	out->height = image.height;
 	out->maxGrayLevel = image.maxGrayLevel;
 	
-	out->imageData = (int**) calloc(out->width, sizeof(int*));
-	for(i = 0; i < out->width; i++) {
-		out->imageData[i] = calloc(out->height, sizeof(int));
+	out->imageData = (int**) calloc(out->height, sizeof(int*));
+	for(i = 0; i < out->height; i++) {
+		out->imageData[i] = calloc(out->width, sizeof(int));
 	}
 	
-	for(i = 0; i < out->width; i++) {
-		for(j = 0; j < out->height; j++) {
+	out->gx = (int**) calloc(out->height, sizeof(int*));
+	for(i = 0; i < out->height; i++) {
+		out->gx[i] = calloc(out->width, sizeof(int));
+	}
+	
+	out->gy = (int**) calloc(out->height, sizeof(int*));
+	for(i = 0; i < out->height; i++) {
+		out->gy[i] = calloc(out->width, sizeof(int));
+	}
+	
+	for(i = 0; i < out->height; i++) {
+		for(j = 0; j < out->width; j++) {
 			out->imageData[i][j] = image.imageData[i][j];
+			out->gx[i][j] = image.imageData[i][j];
+			out->gy[i][j] = image.imageData[i][j];
 		};
 	}
 }
@@ -47,7 +61,6 @@ void read_comments(FILE *input_image) {
 
 void read_pgm_file(char* dir, pgm* image) {
 	FILE* input_image; 
-	char ch;
 	int i, j, num;
 
 	input_image = fopen(dir, "r");
@@ -61,29 +74,51 @@ void read_pgm_file(char* dir, pgm* image) {
 
 	fscanf(input_image, "%d %d %d", &image->width, &image->height, &image->maxGrayLevel);
 	
-	image->imageData = (int**) calloc(image->width, sizeof(int*));
-	for(i = 0; i < image->width; i++) {
-		image->imageData[i] = calloc(image->height, sizeof(int));
+	image->imageData = (int**) calloc(image->height, sizeof(int*));
+	for(i = 0; i < image->height; i++) {
+		image->imageData[i] = calloc(image->width, sizeof(int));
+
 	}
+	
 	if (strcmp(image->version, "P2") == 0) {
 		for (i = 0; i < image->height; i++) {
 			for (j = 0; j < image->width; j++) {
 				fscanf(input_image, "%d", &num);
-				image->imageData[j][i] = num;
+				image->imageData[i][j] = num;
 			}
 		}	
 	}
 	else if (strcmp(image->version, "P5") == 0) {
-		for (i = 0; i < image->height; i++) {
-			for (j = 0; j < image->width; j++) {
-                ch = fgetc(input_image);
-                image->imageData[j][i] = ch;
-            }
-        }
+		char *buffer;
+		int buffer_size = image->height * image->width;
+		buffer = (char*) malloc( ( buffer_size + 1) * sizeof(char));
+		
+		if(buffer == NULL) {
+			printf("Can not allocate memory for buffer! \n");
+			return;
+		}
+		fread(buffer, sizeof(char), image->width * image-> height, input_image);
+		for (i = 0; i < image->height * image ->width; i++) {
+			image->imageData[i / (image->width)][i % image->width] = buffer[i];
+		}
+		free(buffer);
 	}
+	fclose(input_image);
 	printf("_______________IMAGE INFO__________________\n");
 	printf("Version: %s \nWidth: %d \nHeight: %d \nMaximum Gray Level: %d \n", image->version, image->width, image->height, image->maxGrayLevel);
+}
 
+void padding(pgm* image) {
+	int i;
+	for (i = 0; i < image->width; i++) {
+		image->imageData[0][i] = 0;
+		image->imageData[image->height - 1][i] = 0;
+	}
+	
+	for (i = 0; i < image->height; i++) {
+		image->imageData[i][0] = 0;
+		image->imageData[i][image->width - 1] = 0;
+	} 
 }
 
 int convolution(pgm* image, int kernel[3][3], int row, int col) {
@@ -97,7 +132,7 @@ int convolution(pgm* image, int kernel[3][3], int row, int col) {
 }
 
 void sobel_edge_detector(pgm* image, pgm* out_image) {
-	int i, j, x, y;
+	int i, j, gx, gy;
 	int mx[3][3] = {
 		{-1, 0, 1},
 		{-2, 0, 2},
@@ -109,24 +144,55 @@ void sobel_edge_detector(pgm* image, pgm* out_image) {
 		{1, 2, 1}
 	};
 	
-	for (i = 1; i < image->width - 2; i++) {
-		for (j = 1; j < image->height - 2; j++) {
-			x = convolution(image, mx, i, j);
-			y = convolution(image, my, i, j);
-			out_image->imageData[i][j] = sqrt(x*x + y*y);
+	for (i = 1; i < image->height - 2; i++) {
+		for (j = 1; j < image->width - 2; j++) {
+			gx = convolution(image, mx, i, j);
+			gy = convolution(image, my, i, j);
+			out_image->imageData[i][j] = sqrt(gx*gx + gy*gy);
+			out_image->gx[i][j] = gx;
+			out_image->gy[i][j] = gy;
 		}
 	}
 	
 }
 
-void write_pgm_file(pgm* image, char* dir) {
+void min_max_normalization(pgm* image, int** matrix) {
+	int min = 1000000, max = 0, i, j;
+	
+	for(i = 0; i < image->height; i++) {
+		for(j = 0; j < image->width; j++) {
+			if (matrix[i][j] < min) {
+				min = matrix[i][j];
+			}
+			else if (matrix[i][j] > max) {
+				max = matrix[i][j];
+			}
+		}
+	}
+	
+	for(i = 0; i < image->height; i++) {
+		for(j = 0; j < image->width; j++) {
+			double ratio = (double) (matrix[i][j] - min) / (max - min);
+			matrix[i][j] = ratio * 255;
+		}
+	} 
+}
+
+void write_pgm_file(pgm* image, char dir[], int** matrix, char name[]) {
 	FILE* out_image;
 	int i, j, count = 0;
 	
-	char* token = strtok(dir, ".");
+	char* token = strtok(dir, "_");
 	if (token != NULL) {
-		strcat(token, "_output.pgm");
+		strcat(token, name);
 		out_image = fopen(token, "wb");
+	}
+	else {
+		token = strtok(dir, ".");
+		if (token != NULL) {
+			strcat(token, name);
+			out_image = fopen(token, "wb");
+		}
 	}
 	
 	out_image = fopen(dir, "wb");
@@ -137,7 +203,7 @@ void write_pgm_file(pgm* image, char* dir) {
 	if (strcmp(image->version, "P2") == 0) {
 		for(i = 0; i < image->height; i++) {
 			for(j = 0; j < image->width; j++) {
-				fprintf(out_image,"%d", image->imageData[j][i]);
+				fprintf(out_image,"%d", matrix[i][j]);
 				if (count % 17 == 0) 
 					fprintf(out_image,"\n");
 				else 
@@ -149,24 +215,42 @@ void write_pgm_file(pgm* image, char* dir) {
 	else if (strcmp(image->version, "P5") == 0) {
 		for(i = 0; i < image->height; i++) {
 			for(j = 0; j < image->width; j++) {
-				char num = image->imageData[j][i] & 0xFF;
+				char num = matrix[i][j];
 				fprintf(out_image,"%c", num);
 			}
 		} printf("\nImage saved in %s \n", token);
-	}
+	} printf("\n");
+	
+	fclose(out_image);
 }
 
 int main(int argc, char **argv)
 {
 	pgm image, out_image;
-	char dir[100] = "C:/Users/fzehr/Desktop/dla.ascii.pgm";
-	/*printf("Enter the file name: ");
+	char dir[150] = "C:/Users/fzehr/Desktop/brain.ascii.pgm";
+	/*char dir[100];
+	printf("Enter the file name: ");
 	scanf("%s", dir);*/
 	
 	read_pgm_file(dir, &image);
+	padding(&image);
 	init_out_image(&out_image, image);
 	sobel_edge_detector(&image, &out_image);	
-	write_pgm_file(&out_image, dir);
+	
+	min_max_normalization(&out_image, out_image.imageData);
+	min_max_normalization(&out_image, out_image.gx);
+	min_max_normalization(&out_image, out_image.gy);
+
+	write_pgm_file(&out_image, dir, out_image.imageData, "_G.pgm");
+	printf("%s ", dir);
+	write_pgm_file(&out_image, dir, out_image.gx, "_GX.pgm");
+	printf("%s ", dir);
+	write_pgm_file(&out_image, dir, out_image.gy, "_GY.pgm");
+
+	free(image.imageData);
+	free(out_image.imageData);
+	free(out_image.gx);
+	free(out_image.gy);
 	
 	return 0;
 }
